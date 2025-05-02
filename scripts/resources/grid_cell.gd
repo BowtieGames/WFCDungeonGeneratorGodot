3a660bcbc143ed2625b08852a218e32ea98ce9b1
BowtieGames <gamesbowtie@gmail.com>
2025-05-02 16:58:57 -0500

    Created Cell Resources

---
+++ scripts/resources/grid_cell.gd
@@ -0,0 +1,49 @@
+## Resource for holding Cell data
+class_name Cell extends Resource
+
+## List of Faces
+enum Face {
+	NORTH,	#0
+	EAST,	#1
+	SOUTH,	#2
+	WEST	#3
+}
+
+## List of Terrains
+enum Terrain {
+	GRASS,	#0
+	ROAD,	#1
+	SAND,	#2
+	WATER	#3
+}
+
+@export var img: Texture2D
+
+@export var north_socket: Array[Terrain] = [
+	Terrain.GRASS, Terrain.GRASS, Terrain.GRASS
+]
+
+@export var east_socket: Array[Terrain] = [
+	Terrain.GRASS, Terrain.GRASS, Terrain.GRASS
+]
+
+@export var south_socket: Array[Terrain] = [
+	Terrain.GRASS, Terrain.GRASS, Terrain.GRASS
+]
+
+@export var west_socket: Array[Terrain] = [
+	Terrain.GRASS, Terrain.GRASS, Terrain.GRASS
+]
+
+## Sockets ordered clockwise
+var faces : Array[Array] = [
+	north_socket, east_socket, south_socket, west_socket
+]
+
+## Checks if this face matches the other's socket
+## Other should be the other cell's list of faces
+## Face should be the face on self that we are matching with
+func match(other: Array[Array], face: Face) -> bool:
+	var reverse: Array[Terrain] = other[face].duplicate()
+	reverse.reverse()
+	return self.faces[face] == reverse
---
+++ scripts/wfcgenerator.gd
@@ -0,0 +1,14 @@
+class_name WFCGenerator extends TileMapLayer
+
+@export var cell_set: Array[Cell]
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	#get_neighbor_cell(coords: Vector2i, neighbor: TileSet.CellNeighbor)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
